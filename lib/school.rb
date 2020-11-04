c8486ab933d22c5aa1b10b1580f5aa3befb67c96
Jason Whitlock <jrwhitlock7@gmail.com>
2020-11-03 18:20:02 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+
+    
+
+    def initialize(name) #user inputs the school name
+       @name = name
+       @roster = {}
+        
+    end
+
+    def
+        @roster
+    end
+
+    
+
+
+    def add_student(name, grade)
+        @roster[:student] = []
+        @roster[:student] << name grade
+
+
+    end
+
+end
